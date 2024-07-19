b7c45c5b25c9a21e0930a8a71cae2a2d3b015dd5
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-19 08:56:36 +0100

    Update Auth.jsx

--- membership/src/Component/Auth.jsx
+++ membership/src/Component/Auth.jsx
@@ -40,7 +40,7 @@ if(navigate){
     <section className="bg-gray-50 w-full  h-screen dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" className="flex items-center mb-6 text-xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+          <img className="w-12 h-12 mr-2" src="https://i.imgur.com/atBPDbj.jpeg" alt="logo" />
           Slaughter to prevail    
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -68,7 +68,6 @@ if(navigate){
                       </div>
                       <a href="#" className="text-sm text-gray-900 font-medium text-primary-600 hover:underline dark:text-gray-200">Forgot password?</a>
                   </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       {login ? "Don’t have an account yet?" : "Already have an account"} <span onClick={() => {setLogin(prev => !prev); setLogging(false)}} className="font-medium cursor-pointer text-primary-600 hover:underline dark:text-primary-500">{login ? "Sign up" : 'Log in'}</span>
                   </p>
