1cb53b9cc203c16395824f8decf2c194843af895
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-19 07:19:29 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -37,7 +37,7 @@ if(!loggedUser){
   return (
     
 <div className='w-screen h-full'>
-<div style={{backgroundImage:"url(https://media.istockphoto.com/id/1317441636/photo/portrait-of-young-beautiful-girl-in-headphones-isolated-over-dark-background.jpg?s=612x612&w=0&k=20&c=SgJMZnqE1pXVUxnzbmiwm5LA72qOLGIp-aU2cvV2crs=)"}} className="text-center mt-12 mb-8 relative w-full h-[20rem] bg-gradient-to-tr from-gray-900 to-blue-900 rounded-lg md:p-8 dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
+<div style={{backgroundImage:"url(https://media.istockphoto.com/id/1317441636/photo/portrait-of-young-beautiful-girl-in-headphones-isolated-over-dark-background.jpg?s=612x612&w=0&k=20&c=SgJMZnqE1pXVUxnzbmiwm5LA72qOLGIp-aU2cvV2crs=)"}} className="text-center mt-12 mb-8 relative w-[90%] mx-auto h-[20rem] bg-gradient-to-tr from-gray-900 to-blue-900 rounded-lg md:p-8 dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
 <div className='w-full relative h-full '>
       <h2 data-aos="fade-up" className="mb-3 text-xl mx-auto bg-gradient-to-tl from-blue-600 to-white text-transparent bg-clip-text max-w-md md:text-2xl font-extrabold tracking-normal pt-7 text-gray-700">Welcome to Alex Terrible's VIP fan membership Card Signup</h2>
             <p data-aos="fade-down" className="mb-3 w-full font-bold translate-x-[50%] bg-gradient-to-l from-blue-800 to-white text-transparent bg-clip-text text-xs bottom-0 absolute">
