e5cc0dbf947f8db3251a1e53d6c64970c6a44d95
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-19 06:47:50 +0100

    Update Auth.jsx

--- membership/src/Component/Auth.jsx
+++ membership/src/Component/Auth.jsx
@@ -66,7 +66,7 @@ if(navigate){
                             <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <a href="#" className="text-sm text-gray-900 font-medium text-primary-600 hover:underline dark:text-gray-200">Forgot password?</a>
                   </div>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
