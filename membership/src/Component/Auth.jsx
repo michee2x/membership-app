6287c78270fa7d503c4dc2bac4cb83e18db7dea4
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-19 06:44:55 +0100

    Update Auth.jsx

--- membership/src/Component/Auth.jsx
+++ membership/src/Component/Auth.jsx
@@ -37,7 +37,7 @@ if(navigate){
   return <Navigate to={"/"} />
 }
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50 w-full  h-screen dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" className="flex items-center mb-6 text-xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
