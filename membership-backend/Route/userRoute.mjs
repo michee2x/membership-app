c7c52aacfcfe49e7bfaba8b121837bceaeb7edee
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-20 00:40:00 +0100

    Update userRoute.mjs

--- membership-backend/Route/userRoute.mjs
+++ membership-backend/Route/userRoute.mjs
@@ -2,9 +2,11 @@ import express from "express"
 import multer from "multer"
 const route = express.Router()
 const uploadMiddleware = multer({dest:"uploads/"})
-import { editprofile } from "../Controllers/userController.mjs"
+import { editprofile, userProfile } from "../Controllers/userController.mjs"
 import {ProtectedRoute} from "../MiddleWare/ProtectedRoute.mjs"
 
 route.post("/editprofile", uploadMiddleware.single("img"), ProtectedRoute, editprofile)
+
+route.get("/userprof", ProtectedRoute, userProfile)
 
 export default route
