461f75942a0e6e5b702c337e087a812c72fde440
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-19 09:54:38 +0100

    Update Auth.jsx

--- membership/src/Component/Auth.jsx
+++ membership/src/Component/Auth.jsx
@@ -46,7 +46,7 @@ if(navigate){
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl mb-10 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  {login ? "Login" : "Sign"} in to your account
+                  {login ? "Login to your account" : "Sign up to slaughter to prevail"}
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={Login_Signin}>
                   <div>
