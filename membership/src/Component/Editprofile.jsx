e5469fd1db151a43ff31942cd286d5c7a4bbc60a
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-20 01:23:21 +0100

    Update Editprofile.jsx

--- membership/src/Component/Editprofile.jsx
+++ membership/src/Component/Editprofile.jsx
@@ -3,7 +3,7 @@ import {MdPerson} from "react-icons/md"
 import { Navigate } from 'react-router-dom'
 
 const Editprofile = () => {
-
+  const [editing, setEditing] = useState(false)
   const fileRef = useRef(null)
   const [file, setFile] = useState("")
   const [img, setImg] = useState("")
@@ -95,7 +95,7 @@ const Editprofile = () => {
 <textarea id="message" value={data.bio} onChange={(e) => setData({...data, bio:e.target.value})} rows="4" class="block mb-5 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 
   
-  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit profile</button>
+  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => setEditing(true)}>{editing ? editing... : Edit profile}</button>
 </form>
 
     </div>
