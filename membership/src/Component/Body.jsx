dbb1dd05aadd16690866e0b95c12c68dfed841d1
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-08-11 18:20:18 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -98,7 +98,7 @@ return (
         </div>
         <div className="flex items-center justify-between">
             <span data-aos="flip-left" className="text-3xl font-bold text-gray-900 dark:text-white">${i.price}</span>
-            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">purchase max</a>
+            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">PayPal</a>
         </div>
     </div>
 </div>
