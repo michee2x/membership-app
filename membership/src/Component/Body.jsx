f1915b4d860429e01c9d999e9cf81ea8cf72da6b
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-08-11 17:43:36 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -27,9 +27,9 @@ const [data, setData] = useState([
 ])
 
 const [max, setMax] = useState([
-{name:"gold max", price:200, img:"https://i.imgur.com/gJ6rCv8.png"},
-{name:"silver max", price:170, img:"https://i.imgur.com/oSAluGw.png"},
-{name:"white max", price:150, img:"https://i.imgur.com/dG7qVJa.png"},
+{name:"gold max", price:200, img:"https://i.imgur.com/gJ6rCv8.png", rating:5.0},
+{name:"silver max", price:170, img:"https://i.imgur.com/oSAluGw.png", rating:4.2},
+{name:"white max", price:150, img:"https://i.imgur.com/dG7qVJa.png", rating:3.0},
 ])
 
 useEffect(() => {
@@ -61,7 +61,7 @@ if(!loggedUser){
     </div>
 
 
-<p class="max-w-lg text-3xl font-semibold leading-relaxed text-gray-900 dark:text-white">purchase max</p>
+<p class="max-w-lg mx-auto py-16 text-3xl font-semibold leading-relaxed text-gray-900 dark:text-white">purchase max</p>
 
 
 
@@ -70,7 +70,7 @@ return (
 
 <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img className="p-8 rounded-t-lg" src={i.img} alt="max image" />
+        <img data-aos="fade" className="p-8 rounded-t-lg" src={i.img} alt="max image" />
     </a>
     <div className="px-5 pb-5">
         <a href="#">
@@ -94,7 +94,7 @@ return (
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
             </div>
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{i.rating}</span>
         </div>
         <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-gray-900 dark:text-white">${i.price}</span>
