c25092e47702d36fec13494453f9d00a9f49fd48
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-08-12 14:34:24 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -126,9 +126,9 @@ return (
 <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
 
 <div class="flex my-10 flex-col items-center pb-10">
-        <img class="w-40 h-40 mb-3 shadow-lg" src="https://i.imgur.com/SvzAO24.jpeg" alt="Bonnie image"/>
-        <h5 class="mb-1 text-2xl font-medium text-gray-900 dark:text-white">Gold Max</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Rating:{obj ? "5.0" : "0.0"}</span>
+        <img class="w-40 h-40 mb-3 shadow-lg" src={obj && obj.img} alt="Bonnie image"/>
+        <h5 class="mb-1 text-2xl font-medium text-gray-900 dark:text-white">{obj.name}</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">Rating:{obj && obj.rating}</span>
         <div class="flex mt-4 md:mt-6">
             <a onClick={() => setShow(false)} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">go back</a>
             
@@ -138,7 +138,7 @@ return (
 <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
 Choose payment
 </h5>
-<p className="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
+<p className="text-sm font-normal text-gray-500 dark:text-gray-400">Choose with payment medium to purchase {obj && obj.name}.</p>
 <ul className="my-4 space-y-3">
 <li>
 <a className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
