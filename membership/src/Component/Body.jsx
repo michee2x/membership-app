decabb7b9535ba8c4688fe8bfaa4dbd5a4017b09
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-08-12 22:00:38 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -126,7 +126,7 @@ return (
 <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
 
 <div class="flex my-10 flex-col items-center pb-10">
-        <img class="w-60 h-60 mb-3 shadow-lg" src={obj && obj.img} alt="Bonnie image"/>
+        <img data-aos="zoom-in" className="p-8 rounded-t-lg" src={obj && obj.img} alt="max image" />
         <h5 class="mb-1 text-2xl font-medium text-gray-900 dark:text-white">{obj.name}</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">Rating:{obj && obj.rating}</span>
         <div class="flex mt-4 md:mt-6">
