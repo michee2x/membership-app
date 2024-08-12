3f6ab02f1bff9584c5a8f5f887d83ece91ed4bf8
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-08-12 21:53:43 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -130,7 +130,7 @@ return (
         <h5 class="mb-1 text-2xl font-medium text-gray-900 dark:text-white">{obj.name}</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">Rating:{obj && obj.rating}</span>
         <div class="flex mt-4 md:mt-6">
-            <a onClick={() => setShow(false)} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">go back</a>
+            <a onClick={() => setShow(false)} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">home</a>
             
         </div>
     </div>
