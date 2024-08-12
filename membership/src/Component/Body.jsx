6f071d297370e68b6ef26fbf9bd04e7412a0b0b0
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-08-12 13:45:51 +0100

    Update Body.jsx

--- membership/src/Component/Body.jsx
+++ membership/src/Component/Body.jsx
@@ -137,8 +137,6 @@ Choose payment
 </a>
 </li>
 
-
-</li>
 <li>
 <a class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
 <FaPaypal
