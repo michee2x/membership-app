b20c42b94d2b1084f8b54c724fb2e31dd07a5dce
Michael Israel <157503371+michee2x@users.noreply.github.com>
2025-04-09 01:38:09 +0100

    Update Footer.jsx

--- membership/src/Component/Footer.jsx
+++ membership/src/Component/Footer.jsx
@@ -12,7 +12,7 @@ useEffect(() => {
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 hidden" alt="Flowbite Logo" />
-                <span className="self-center text-sm font-semibold whitespace-nowrap dark:text-white">Slaughter to prevail</span>
+                <span className="self-center text-sm font-semibold whitespace-nowrap dark:text-white">Sabaton</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
@@ -30,7 +30,7 @@ useEffect(() => {
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://slaughter-to-prevail.onrender.com" className="hover:underline">slaughter-to-prevail™</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://slaughter-to-prevail.onrender.com" className="hover:underline">Sabaton™</a>. All Rights Reserved.</span>
     </div>
 </footer>
 
