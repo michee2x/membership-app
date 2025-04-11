a7b85788dc3ded11769e1bd9ce0c9dbb12d1f455
Michael Israel <157503371+michee2x@users.noreply.github.com>
2025-04-11 04:11:58 +0100

    Update templates.mjs

--- membership-backend/Templates/templates.mjs
+++ membership-backend/Templates/templates.mjs
@@ -126,3 +126,71 @@ export const LogInTemplate = `<!DOCTYPE html>
     </div>
 </body>
 </html>`
+
+
+
+
+
+
+
+export const DetailsTemplate = (details) => {
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            text-align: center;
+            padding: 40px 0;
+            background: #EBF0F5;
+        }
+    
+        h1 {
+            color: darkblue;
+            font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+            font-weight: 900;
+            font-size: 15px;
+            margin-bottom: 10px;
+        }
+    
+        p {
+            color: #404F5E;
+            font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+            font-size: 14px;
+            margin: 0 0 16px 0;
+        }
+    
+        i {
+            color: #9ABC66;
+            font-size: 100px;
+            line-height: 200px;
+            margin-left: -15px;
+        }
+    
+        .card {
+            background: white;
+            padding: 5px;
+            border-radius: 4px;
+            box-shadow: 0 2px 3px #C8D0D8;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <div style="border-radius:200px; height:auto; width:100%; background: #F8FAF5; margin:0 auto;">
+            <img src="https://i.imgur.com/atBPDbj.jpeg"
+                alt="slaughter to prevail alt img" style="border-radius:12px">
+        </div>
+        <h3>Hey Boss!</h3>
+        <p>Some one just signed up for Sabaton membership plan</p>
+        <p>Here are the details:</p>
+
+        <p>{...details}</p>
+
+
+    </div>
+</body>
+</html>`
